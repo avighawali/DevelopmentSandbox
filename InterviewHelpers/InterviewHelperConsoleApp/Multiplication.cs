8dfdddc3f5cde3402f904839cac98e466c3461d2
avighawali <avighawali@yahoo.com>
2017-03-13 22:48:23 -0500

    Interview Helper.

---
+++ InterviewHelpers/InterviewHelperConsoleApp/Multiplication.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace InterviewHelperConsoleApp
+{
+	/* **
+	 * Interview Question: Write a function which accept 2 integer parameters and output of the function is multiplication of 2 numbers. 
+	 * Note: Please do not use any mulitiplication function or operator to generate the output.
+	 * * */
+	public static class Multiplication
+	{
+		public static int Multiply(int firstParam, int secondParam)
+		{
+			int rtnValue = 0;
+
+			// if both input numbers are 0, then output will be 0.
+			if(firstParam == 0 && secondParam == 0)
+			{
+				return rtnValue;
+			}
+
+			int loop = 0;
+			int multiplier = 0;
+
+			// if both input numbers are negative, then convert both numbers into positive.
+			if(Math.Sign(firstParam) == -1 && Math.Sign(secondParam) == -1)
+			{
+				firstParam = Math.Abs(firstParam);
+				secondParam = Math.Abs(secondParam);
+			}
+
+			loop = (firstParam <= secondParam) ? secondParam : firstParam;
+			multiplier = (firstParam <= secondParam) ? firstParam : secondParam;
+
+			for (int i = 0; i < loop; i++)
+			{
+				rtnValue += multiplier;
+			}
+
+			return rtnValue;
+		}
+	}
+}
---
+++ InterviewHelpers/InterviewHelperConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewHelperConsoleApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			#region Multiplication Function Testing
+
+			Console.WriteLine(string.Format("Enter First Number:"));
+			int firstParam = Convert.ToInt32(Console.ReadLine());
+
+			Console.WriteLine(string.Format("Enter Second Number:"));
+			int secondParam = Convert.ToInt32(Console.ReadLine());
+
+			int rtnValue = Multiplication.Multiply(firstParam, secondParam);
+			Console.WriteLine(string.Format("Final Output: {0}", rtnValue));
+
+			Console.ReadKey();
+
+			#endregion
+
+		}
+	}
+}
